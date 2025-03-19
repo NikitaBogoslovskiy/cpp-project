188cd49d28ce15f5aad2ae9d60b02b3a50723093
Nikita Bogoslovskiy <nikita-beeline@yandex.ru>
2025-03-19 21:31:46 +0300

    add libzip library

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,12 @@
 #include <jni.h>
 #include <string>
+#include <zip.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_cpp_1project_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+
+    std::string message = "libzip library version: " + std::string(zip_libzip_version());
+    return env->NewStringUTF(message.c_str());
 }
